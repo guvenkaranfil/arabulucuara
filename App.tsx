ab3bed9540e9a7d26e07a70d18cf5c4f02c46b0c
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-05-25 22:56:03 +0300

    feat: 'react-native-linear-gradient' library setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'light-content'} />
       <View>
         <Text>App.js</Text>
+        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+          <Text style={styles.buttonText}>Sign in with Facebook</Text>
+        </LinearGradient>
       </View>
     </SafeAreaView>
   );
@@ -17,6 +22,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  linearGradient: {
+    width: 300,
+    height: 40,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
   },
 });
 
