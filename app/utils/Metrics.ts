453874c8c6cda5f82c828dd828b77fe818fc5736
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-01 00:22:19 +0300

    feat: responsive calculator functions 'hp' and 'wp' created based on design height and width.

--- app/utils/Metrics.ts
+++ app/utils/Metrics.ts
@@ -1,7 +1,20 @@
 import {Dimensions} from 'react-native';
+import {widthPercentageToDP, heightPercentageToDP} from 'react-native-responsive-screen';
 
 const {width, height} = Dimensions.get('window');
+
+function widthPercentageCalculator(heightNum: number) {
+  return widthPercentageToDP((heightNum * 100) / 375);
+}
+
+function heightPercentageCalculator(heightNum: number) {
+  return heightPercentageToDP((heightNum * 100) / 812);
+}
+
 export default {
   DEVICE_WIDTH: width,
   DEVICE_HEIGHT: height,
+
+  wp: widthPercentageCalculator,
+  hp: heightPercentageCalculator,
 };
