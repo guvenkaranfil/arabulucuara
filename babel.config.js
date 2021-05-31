faec4056a13954c1792647c1ae31cbbe71085b92
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-05-31 23:10:46 +0300

    module resolver for @auth, @components, @utils, @icons.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./app'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@auth': './app/container/auth',
+          '@components': './app/components',
+          '@utils': './app/utils',
+          '@icons': './app/icons',
+        },
+      },
+    ],
+  ],
 };
