d50cd78dfce527925264a2aa189db8229fa56fd9
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-01 22:30:58 +0300

    refactor: remove auth/login extra folders for login screens.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
-import Login from '@auth/login/Login';
+import Login from '@auth/Login';
 
 const App = () => {
   return (
