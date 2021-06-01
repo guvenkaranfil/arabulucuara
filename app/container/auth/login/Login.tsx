32e0545fcd85249eff5f0eee0d48532fb013e496
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-01 22:11:26 +0300

    feat: add ScrollView to protect responsiveness for smaller devices which is not put with design specs height calculations.

--- app/container/auth/login/Login.tsx
+++ app/container/auth/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import LoginLayout from '@components/layouts/LoginLayout';
 import Header from '@components/auth/Header';
 import AuthInput from '@components/input/AuthInput';
@@ -14,43 +14,45 @@ export default function Login() {
 
   return (
     <LoginLayout showBackButton={true}>
-      <Header screenTitle="Üye Girişi" dynamicHeight={200} />
+      <ScrollView bounces={false}>
+        <Header screenTitle="Üye Girişi" dynamicHeight={200} />
 
-      <View style={styles.inputs}>
-        <AuthInput
-          value={email}
-          onChangeText={setemail}
-          placeholder="E-Posta"
-          keyboardType="email-address"
-        />
-        <AuthInput
-          value={password}
-          onChangeText={setpassword}
-          placeholder="Şifre"
-          isPassowordInput={true}
-          secureTextEntry={hidePassword}
-          onPressSecure={() => sethidePassword(prev => !prev)}
-        />
-      </View>
-
-      <View style={styles.forgotPassword}>
-        <TouchableOpacity>
-          <Text style={styles.forgotPasswordText}>Şifremi unuttum</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.signIn}>
-        <FilledButton label="Giriş Yap" onPress={() => console.log('onPress..')} />
-      </View>
-
-      <View style={styles.footer}>
-        <View style={styles.footerLabel}>
-          <Text style={styles.footerLabelText}>{dashedLine}</Text>
-          <Text style={styles.footerLabelText}>veya</Text>
-          <Text style={styles.footerLabelText}>{dashedLine}</Text>
+        <View style={styles.inputs}>
+          <AuthInput
+            value={email}
+            onChangeText={setemail}
+            placeholder="E-Posta"
+            keyboardType="email-address"
+          />
+          <AuthInput
+            value={password}
+            onChangeText={setpassword}
+            placeholder="Şifre"
+            isPassowordInput={true}
+            secureTextEntry={hidePassword}
+            onPressSecure={() => sethidePassword(prev => !prev)}
+          />
         </View>
-        <OutlineButton label="Üye Ol" onPress={() => console.log('onPress..')} />
-      </View>
+
+        <View style={styles.forgotPassword}>
+          <TouchableOpacity>
+            <Text style={styles.forgotPasswordText}>Şifremi unuttum</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.signIn}>
+          <FilledButton label="Giriş Yap" onPress={() => console.log('onPress..')} />
+        </View>
+
+        <View style={styles.footer}>
+          <View style={styles.footerLabel}>
+            <Text style={styles.footerLabelText}>{dashedLine}</Text>
+            <Text style={styles.footerLabelText}>veya</Text>
+            <Text style={styles.footerLabelText}>{dashedLine}</Text>
+          </View>
+          <OutlineButton label="Üye Ol" onPress={() => console.log('onPress..')} />
+        </View>
+      </ScrollView>
     </LoginLayout>
   );
 }
