55e1d10df230719d2163d79f5448d4e8b60e1f65
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-02 14:50:47 +0300

    feat: 'Register' screen form inputs

--- app/container/auth/Register.tsx
+++ app/container/auth/Register.tsx
@@ -1,10 +1,59 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, View, StyleSheet} from 'react-native';
+import LoginLayout from '@components/layouts/LoginLayout';
+import Header from '@components/auth/Header';
+import AuthInput from '@components/input/AuthInput';
 
 export default function Register() {
+  const [name, setname] = useState('');
+  const [surname, setsurname] = useState('');
+  const [email, setemail] = useState('');
+  const [registrationNumber, setregistrationNumber] = useState('');
+  const [username, setusername] = useState('');
+  const [password, setpassword] = useState('');
+  const [hidePassword, sethidePassword] = useState(true);
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <LoginLayout showBackButton={true}>
+      <ScrollView>
+        <Header screenTitle="Arabulucu Üye Ol" dynamicHeight={200} />
+
+        <View style={styles.inputs}>
+          <AuthInput value={name} onChangeText={setname} placeholder="İsim" />
+          <AuthInput value={surname} onChangeText={setsurname} placeholder="Soyisim" />
+
+          <AuthInput
+            value={email}
+            onChangeText={setemail}
+            placeholder="E-Posta"
+            keyboardType="email-address"
+          />
+
+          <AuthInput
+            value={registrationNumber}
+            onChangeText={setregistrationNumber}
+            placeholder="Arabulucu Sicil No"
+          />
+
+          <AuthInput value={username} onChangeText={setusername} placeholder="Kullanıcı Adı" />
+
+          <AuthInput
+            value={password}
+            onChangeText={setpassword}
+            placeholder="Şifre"
+            isPassowordInput={true}
+            secureTextEntry={hidePassword}
+            onPressSecure={() => sethidePassword(prev => !prev)}
+          />
+        </View>
+      </ScrollView>
+    </LoginLayout>
   );
 }
+
+const styles = StyleSheet.create({
+  inputs: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+});
