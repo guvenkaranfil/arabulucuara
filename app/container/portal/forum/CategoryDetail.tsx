625da787cc6c1931e02eb45109a5df5d6668b505
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-05 01:25:21 +0300

    feat: 'categoryDetail' screen design

--- app/container/portal/forum/CategoryDetail.tsx
+++ app/container/portal/forum/CategoryDetail.tsx
@@ -1,12 +1,132 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {RouteProp} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {PortalNavigatorParamList} from 'routes/stacks/portal/Types';
+import {Fonts, Metrics} from 'utils';
+import Topic from './components/Topic';
+import FilledButton from 'components/buttons/FilledButton';
 
-export default function CategoryDetail() {
+export interface Props {
+  route: RouteProp<PortalNavigatorParamList, 'categoryDetail'>;
+  navigation: StackNavigationProp<PortalNavigatorParamList, 'categoryDetail'>;
+}
+
+export default function CategoryDetail({route, navigation}: Props) {
+  const {label} = route.params;
+
+  const _renderHeader = () => (
+    <View style={styles.header}>
+      <Text style={styles.title}>{label}</Text>
+    </View>
+  );
+
+  const _renderAddNewTopicButton = () => (
+    <FilledButton label="Yeni Konu" bgColor="#7E0736" onPress={() => console.log('onPress..')} />
+  );
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <FlatList
+        contentContainerStyle={styles.topics}
+        data={topicSample}
+        ListHeaderComponent={_renderHeader}
+        renderItem={({item}) => <Topic topic={item} />}
+        ListFooterComponent={_renderAddNewTopicButton}
+        ListFooterComponentStyle={styles.footer}
+        keyExtractor={(_, index) => String(index)}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  header: {
+    marginTop: 28,
+    padding: 12,
+    width: Metrics.DEVICE_WIDTH,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F4E1F0',
+  },
+
+  title: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: Fonts.robotoBold,
+    color: '#181C32',
+  },
+
+  topics: {
+    paddingBottom: 30,
+    alignItems: 'center',
+  },
+
+  footer: {
+    marginTop: 30,
+  },
+});
+
+const topicSample = [
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+  {
+    id: 1,
+    date: '16.4.2021',
+    label: 'ARABULUCULUK UZMANLIK EĞİTİMLERİNİN GEREKLİLİĞİ HAKKINDA NE DÜŞÜNÜYORSUNUZ?',
+    nameSurname: 'Murat GÜNEY',
+    views: 152,
+  },
+];
