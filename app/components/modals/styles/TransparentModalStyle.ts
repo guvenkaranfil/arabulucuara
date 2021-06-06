2f4e35556adfc39691c6cbe3d17ba443f2ed8c89
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-07 00:43:48 +0300

    refactor: 'TransparentModal' styles into seperate file

---
+++ app/components/modals/styles/TransparentModalStyle.ts
@@ -0,0 +1,64 @@
+import {StyleSheet} from 'react-native';
+import {Fonts, Metrics} from '@utils';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000aa',
+  },
+
+  centeredContainer: {
+    width: Metrics.DEVICE_WIDTH - 28 * 2,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+  },
+
+  modalHeader: {
+    marginBottom: 15,
+    paddingTop: 26,
+    paddingHorizontal: 21,
+    width: '100%',
+    height: 57,
+    borderBottomWidth: 1,
+    borderBottomColor: '#707070',
+  },
+
+  title: {
+    fontSize: 16,
+    fontFamily: Fonts.robotoLight,
+    color: '#181C32',
+  },
+
+  content: {
+    paddingBottom: 28,
+    paddingHorizontal: 21,
+  },
+
+  footer: {
+    marginBottom: 11,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    width: '100%',
+    height: 70,
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    borderTopWidth: 1,
+    borderTopColor: '#707070',
+  },
+
+  cancelButton: {
+    width: Metrics.wp(112),
+    height: Metrics.hp(44),
+    backgroundColor: '#F4E1F0',
+  },
+
+  cancelLabel: {color: '#7E0736'},
+
+  approveButton: {
+    width: Metrics.wp(112),
+    height: Metrics.hp(44),
+    backgroundColor: '#7E0736',
+  },
+});
