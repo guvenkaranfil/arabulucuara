aae7394fe21abe0e38977fafc75dcad44dcc6b13
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-09 22:28:08 +0300

    refactor: extract topic commet design into seperate component

---
+++ app/container/portal/forum/components/TopicComment.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image, Pressable} from 'react-native';
+import HTML from 'react-native-render-html';
+import {LikeIcon, DislikeIcon} from '@icons';
+import {Fonts, Metrics} from 'utils';
+
+interface TopicComment {
+  comment: {
+    id: number;
+    nameSurname: string;
+    profilePhoto: string;
+    date: string;
+    comment: string;
+    likedCount: number;
+    dislikeCount: number;
+  };
+}
+
+export default function TopicComment({comment}: TopicComment) {
+  return (
+    <View style={styles.comment}>
+      <View style={styles.commentHeader}>
+        <Image source={{uri: comment.profilePhoto}} style={styles.profilePhoto} />
+        <View style={styles.commentByUser}>
+          <Text style={styles.commentTitle}>{comment.nameSurname}</Text>
+          <Text style={styles.commentDate}>{comment.date}</Text>
+        </View>
+      </View>
+
+      <View>
+        <HTML source={{html: comment.comment}} />
+      </View>
+
+      <View style={styles.commentFooter}>
+        <Pressable style={styles.commentAction}>
+          <LikeIcon width={16} height={16} />
+          <Text style={styles.commentCount}>{comment.likedCount}</Text>
+        </Pressable>
+
+        <Pressable style={styles.commentAction}>
+          <DislikeIcon width={16} height={16} />
+          <Text style={styles.commentCount}>{comment.dislikeCount}</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  comment: {
+    marginBottom: 20,
+    padding: 16,
+    width: Metrics.DEVICE_WIDTH - 28 * 2,
+    borderRadius: 10,
+    backgroundColor: '#F5F6FA',
+  },
+
+  commentHeader: {
+    marginBottom: 17,
+    flexDirection: 'row',
+  },
+
+  profilePhoto: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    resizeMode: 'contain',
+  },
+
+  commentByUser: {
+    marginLeft: 9,
+  },
+
+  commentTitle: {
+    fontSize: 16,
+    fontFamily: Fonts.robotoBold,
+    color: '#181C32',
+  },
+
+  commentDate: {
+    paddingTop: 3,
+    fontSize: 12,
+    fontFamily: Fonts.robotoRegular,
+    color: '#B3B3B3',
+  },
+
+  commentFooter: {
+    marginTop: 22,
+    paddingTop: 10,
+    paddingRight: 35,
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    borderTopWidth: 1,
+    borderTopColor: '#B3B3B3',
+  },
+
+  commentAction: {
+    marginLeft: 17,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  commentCount: {
+    paddingLeft: 9,
+    fontSize: 13,
+    fontFamily: Fonts.robotoRegular,
+    color: '#181C32',
+  },
+});
