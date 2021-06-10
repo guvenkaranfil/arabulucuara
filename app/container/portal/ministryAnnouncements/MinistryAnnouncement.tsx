389b8c82f2d5d469214d799f00ed1f62c75176a1
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-10 21:40:15 +0300

    feat: 'MinistryAnnouncementDetail' screen design

--- app/container/portal/ministryAnnouncements/MinistryAnnouncement.tsx
+++ app/container/portal/ministryAnnouncements/MinistryAnnouncement.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {RouteProp} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import HTML from 'react-native-render-html';
 
-export default function MinistryAnnouncement() {
+import {content} from '../forum/mocks';
+import {PortalNavigatorParamList} from '@routes/stacks/portal/Types';
+import {CommonStyles, Fonts} from '@utils';
+
+export interface Props {
+  route: RouteProp<PortalNavigatorParamList, 'ministryAnnouncement'>;
+  navigation: StackNavigationProp<PortalNavigatorParamList, 'ministryAnnouncement'>;
+}
+
+export default function MinistryAnnouncement({route}: Props) {
+  const {title} = route.params;
+
   return (
-    <View>
-      <Text>MinistryAnnouncement</Text>
+    <View style={CommonStyles.container}>
+      <ScrollView style={CommonStyles.paddingForScroll}>
+        <Text style={styles.title}>{title}</Text>
+
+        <HTML source={{html: content.htmlContent}} containerStyle={styles.articleContent} />
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 16,
+    fontFamily: Fonts.robotoBold,
+    color: '#181C32',
+  },
+
+  articleContent: {marginVertical: 25},
+});
