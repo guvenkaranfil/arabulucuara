455162edad3b4b43a058b5e2dbae7384f0028b03
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-13 19:00:11 +0300

    fix: import utils style

--- app/container/home/index.tsx
+++ app/container/home/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ForLoggedUser from './ForLoggedUser';
 import ForNonLoggedUser from './ForNonLoggedUser';
 
-const isUserLoggedIn = false;
+const isUserLoggedIn = true;
 export default function Index() {
   return isUserLoggedIn ? <ForLoggedUser /> : <ForNonLoggedUser />;
 }
