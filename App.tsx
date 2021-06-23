bd0e1a18d94f65102c20b4a44f3036e607d1af6e
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-06-23 17:19:10 +0300

    fix: import aliase

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
-import AppNavigator from 'routes/AppNavigator';
+import AppNavigator from '@routes/AppNavigator';
 
 const App = () => {
   return (
