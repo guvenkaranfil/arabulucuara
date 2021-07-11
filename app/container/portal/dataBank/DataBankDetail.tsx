1cd0c3920032b7229418ecab4cf866c7302ae007
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-07-11 19:45:49 +0300

    feat: databank detail - api

--- app/container/portal/dataBank/DataBankDetail.tsx
+++ app/container/portal/dataBank/DataBankDetail.tsx
@@ -4,6 +4,7 @@ import {RouteProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {PortalNavigatorParamList} from '@routes/stacks/portal/Types';
 import HTML from 'react-native-render-html';
+import {WebView} from 'react-native-webview';
 
 import {CommonStyles, Fonts} from '@utils';
 import FilledButton from '@components/buttons/FilledButton';
@@ -22,12 +23,24 @@ export interface DataBankDetail {
 export default function DataBankDetail({route}: Props) {
   const {dataBankDetail} = route.params;
 
+  if (!dataBankDetail?.body) {
+    return (
+      <View style={CommonStyles.container}>
+        <WebView
+          source={{
+            uri: 'https://arabulucuara.com/uploaded/BilgiBankasi/' + dataBankDetail?.path,
+          }}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={CommonStyles.container}>
       <ScrollView contentContainerStyle={[CommonStyles.paddingForScroll]}>
-        <Text style={styles.title}>{dataBankDetail.name}</Text>
+        <Text style={styles.title}>{dataBankDetail.title}</Text>
 
-        <HTML source={{html: dataBankDetail.content}} containerStyle={styles.dataBankDetail} />
+        <HTML source={{html: dataBankDetail?.body}} containerStyle={styles.dataBankDetail} />
 
         <FilledButton
           label="İçeriği Görüntüle"
