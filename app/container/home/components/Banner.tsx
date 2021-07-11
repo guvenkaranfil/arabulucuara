3cc95309f1ff7b47a267ef594f22fb825f3f6bcc
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-07-11 13:24:02 +0300

    feat: Swiper for banner area

--- app/container/home/components/Banner.tsx
+++ app/container/home/components/Banner.tsx
@@ -2,19 +2,37 @@ import {Banner} from '@home/Models';
 import React from 'react';
 import {StyleSheet, Image, View} from 'react-native';
 import {Metrics} from '@utils';
+import Swiper from 'react-native-swiper';
 
 export default function BannerArea({banners}: {banners: Array<Banner>}) {
   return (
     <View style={styles.container}>
-      <Image source={{uri: banners[1]?.image}} style={styles.banner} />
+      <Swiper showsButtons={false} paginationStyle={styles.swiperPaginationStyle}>
+        {banners.map((banner, index) => (
+          <View key={index} style={styles.bannerArea}>
+            <Image source={{uri: banner?.image}} style={styles.banner} />
+          </View>
+        ))}
+      </Swiper>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     width: Metrics.DEVICE_WIDTH - 40,
     height: Metrics.wp(164),
+  },
+
+  swiperPaginationStyle: {
+    position: 'absolute',
+    bottom: 0,
+  },
+
+  bannerArea: {
+    width: Metrics.DEVICE_WIDTH - 40,
+    height: '100%',
   },
 
   banner: {
